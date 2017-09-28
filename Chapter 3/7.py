3fc5e5e2d434ab54a8adf31c592359bbf1045548
ForestMilles <31958674+ForestMilles@users.noreply.github.com>
2017-09-28 11:39:20 -0400

    corrections to 7.py

--- Chapter 3/7.py
+++ Chapter 3/7.py
@@ -5,6 +5,6 @@ wn= turtle.Screen()
 turtle.color("orange")
 turtle.pendown()
 for turn in [160, -43, 270, -97, -43, 200, -940, 17, -86]:
-    turtle.left(turn)
+    turtle.right(turn)
     turtle.forward(100)
-print(turtle.position())
+print(turtle.heading())
